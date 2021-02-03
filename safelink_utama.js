fcd1a4e4087c7135524f07e3635baa2441d6f590
faturohmane9 <60047789+faturohmane9@users.noreply.github.com>
2021-02-03 09:01:08 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="data-science-and-its-rising-importance.html"}
 if(x=="2"){xxx="leukemia-treatment-breakthroughs-for.html"}
-if(x=="3"){xxx="5-benefits-of-marble-floors.html"}a_to_ve[i].href="https://www.prodesain.net/2020/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="5-benefits-of-marble-floors.html"}a_to_ve[i].href="https://svg-file-download.blogspot.com/2020/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
